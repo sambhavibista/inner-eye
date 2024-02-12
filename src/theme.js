4974c626e37a56a51b7d23e67336144cd93476c4
sambhavibista <samubista18@gmail.com>
2024-02-12 13:07:09 +0545

    navBar added

---
+++ src/theme.js
@@ -0,0 +1,32 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f3ecf1", // YELLOW COLOR
+    },
+    secondary: {
+      main: "#543d72",
+      secondary_300: "#eeeeee",
+      secondary_400: "#480765",
+      secondary_600: "#6a1067", // BLACK COLOR
+    },
+    tertiary: {
+      main: "#f58220", // GREEN COLOR
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 950,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+  typography: {
+    fontFamily: "Roboto serif",
+  },
+});
+
+export default theme;
